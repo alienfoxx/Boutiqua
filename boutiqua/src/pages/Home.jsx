be3b403547493903499b76ec3d54b4e5dad094b5
alienfoxx <alienfoxx001@gmail.com>
2024-04-29 03:10:28 +0100

    add the Navbar Component

---
+++ boutiqua/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../componenet/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+
+      <Navbar/>
+
+    </div>
+  );
+};
+
+export default Home;
